d1fd95d679170dc7862a1d9e239d5a7813eeae1d
tecoscr <tecoscr@gmail.com>
2017-07-19 13:12:33 +0200

    Update glejser.R

--- glejser.R
+++ glejser.R
@@ -11,12 +11,12 @@ glejsertest <- function(model, regressor, power=c(1, 0.5, -0.5,-1)){
     glejser.test[i,2:4] <- f
     glejser.test[i,5] <- pf(f[1],f[2],f[3],lower.tail=F)
   }
-  print("Glejser test", quote=FALSE)
-  print(" ", quote=FALSE)
+  # return(glejser.test)
+  cat("\n Glejser test \n")
   hi <- which(glejser.test[,2] == max(glejser.test[,2]), arr.ind = TRUE)[1]
-  print(paste("Power: ", glejser.test[hi,1], sep=""), quote=FALSE)
-  print(paste("Glejser = ", round(glejser.test[hi,2],3), 
-              ", df1 = ", glejser.test[hi,3], 
-              ", df2 = ", glejser.test[hi,4], 
-              ", p-value = ", round(glejser.test[hi,5],4),sep = ""), quote=FALSE)
+  cat("\nPower = ", glejser.test[hi,1])
+  cat("\nGlejser = ", round(glejser.test[hi,2],3), 
+      ", df1 = ", glejser.test[hi,3], 
+      ", df2 = ", glejser.test[hi,4],
+      ", p-value = ", round(glejser.test[hi,5],4), sep="")
 }
